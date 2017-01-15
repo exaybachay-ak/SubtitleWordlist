5b3be11d248462ab01ca4da23f1078bd839c289b
exaybachay-ak <exaybachay.ak@gmail.com>
2017-01-14 22:16:20 -0900

    Update wordlist.py
    More proofreading and touchup work - removed an if statement and modified another that was set wrong for testing.

--- wordlist.py
+++ wordlist.py
@@ -13,27 +13,26 @@ wordlist = []
 if 'linux' in platform:
 	files = [f for f in os.listdir('.') if os.path.isfile(f)]
 	for f in files:
-		if f != 'translate_curdir.py':
-			if f != 'translate_curdir_new.py':
-				# open input file to read and work with	
-				file = open(f, 'r')
+		if f != 'wordlist.py':
+			# open input file to read and work with	
+			file = open(f, 'r')
 
-				# .lower() returns a version with all upper case characters replaced with lower case characters.
-				text = file.read().lower()
-				file.close()
+			# .lower() returns a version with all upper case characters replaced with lower case characters.
+			text = file.read().lower()
+			file.close()
 
-				# replaces anything that is not a lowercase letter, a space, or an apostrophe with a space:
-				text = re.sub('[^a-z\ \']+', " ", text)
-				text = re.sub('\ ', '\n', text)
-				text = re.sub('\"', '\'', text)
-				words = list(text.split())
-				for w in words:
-					w = w.lstrip('\'')
-					w = w.rstrip(',')
-					w = w.rstrip('\'')
+			# replaces anything that is not a lowercase letter, a space, or an apostrophe with a space:
+			text = re.sub('[^a-z\ \']+', " ", text)
+			text = re.sub('\ ', '\n', text)
+			text = re.sub('\"', '\'', text)
+			words = list(text.split())
+			for w in words:
+				w = w.lstrip('\'')
+				w = w.rstrip(',')
+				w = w.rstrip('\'')
 
-				for word in words:
-					wordlist.append(word)
+			for word in words:
+				wordlist.append(word)
 
 else:
 	print "this OS is Windows..."
